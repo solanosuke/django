8ccc8797ea75fbc16e7112bd088402b637ca3973
aosolayy <aosola.yy@gmail.com>
2022-07-05 16:24:29 +0900

    project2 commit

---
+++ first/project_test/diary/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.utils import timezone  # djangoでは、datetime.now のかわりに、timezone.now で現在日付・時刻を取得する
+
+
+class Day(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField('タイトル', max_length=200)
+    text = models.TextField('本文')
+    date = models.DateTimeField('日付', default=timezone.now)
